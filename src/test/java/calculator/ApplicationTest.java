a1642b839963f0574b9fe6969f9976317cd377ad
JYL35 <dlwodbs1223@naver.com>
2025-10-17 00:31:49 +0900

    test: 기본 구분자 처리, 더하기 테스트 추가

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,27 +1,40 @@
 package calculator;
 
+import calculator.model.Calculator;
+import calculator.service.CalculatorService;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+    CalculatorService calculatorService = new CalculatorService();
+    Calculator calculator = new Calculator();
+
     @Test
-    void 커스텀_구분자_사용() {
-        assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
-        });
+    @DisplayName("기본 구분자 처리 테스트")
+    void findNumberTest() {
+        String input = "1,2:5";
+        List<String> testNumbers = List.of("1", "2", "5");
+
+        List<String> resultNumbers = calculatorService.findNumber(input);
+
+        assertThat(resultNumbers).containsAll(testNumbers);
     }
 
     @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
-        );
+    @DisplayName("더하기 테스트")
+    void addNumberTest() {
+        int testNumber = 8;
+
+        List<String> inputNumber = List.of("1", "2", "5");
+
+        int resultNumber = calculator.addNumber(inputNumber);
+
+        assertThat(resultNumber).isEqualTo(testNumber);
     }
 
     @Override
