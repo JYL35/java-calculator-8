b777c99817a5c5f8d84467d13b424eea204e6db2
JYL35 <dlwodbs1223@naver.com>
2025-10-17 16:03:38 +0900

    feat: 기능 제어를 위한 Controller 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,18 +1,15 @@
 package calculator;
 
+import calculator.controller.CalculatorController;
 import calculator.model.Calculator;
 import calculator.service.CalculatorService;
-
-import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         CalculatorService calculatorService = new CalculatorService();
 
-        List<String> numberList = calculatorService.findNumber("1,2:5");
-        int result = calculator.addNumber(numberList);
-
-        System.out.println(result);
+        CalculatorController calculatorController = new CalculatorController(calculator, calculatorService);
+        calculatorController.calculatorStart();
     }
 }
---
+++ src/main/java/calculator/controller/CalculatorController.java
@@ -0,0 +1,26 @@
+package calculator.controller;
+
+import calculator.model.Calculator;
+import calculator.service.CalculatorService;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class CalculatorController {
+    Calculator calculator;
+    CalculatorService calculatorService;
+
+    public CalculatorController(Calculator calculator, CalculatorService calculatorService) {
+        this.calculator = calculator;
+        this.calculatorService = calculatorService;
+    }
+
+    public void calculatorStart() {
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        List<String> numberList = calculatorService.findNumber(input);
+        int result = calculator.addNumber(numberList);
+
+        System.out.println("결과 : " + result);
+    }
+}
